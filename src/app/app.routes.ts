4dc09fc60c002c150fbdae487034e0e1cba11a9a
ejais58 <ejais58@gmail.com>
2024-10-28 11:53:34 -0300

    reajustes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './features/auth/login/login.component';
-import { RegisterComponent } from './features/auth/register/register.component';
+import { LoginComponent } from './features/auth/login/login-page/login.component';
+import { RegisterComponent } from './features/auth/register/register-page/register.component';
 
 export const routes: Routes = [
   {
